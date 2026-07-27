44e104729ebe07db76d462f0cf9cfac813da3cd9
amnaliya <amnaah207@gmail.com>
2026-07-27 10:14:36 +0530

    Time: 20 ms (37.29%), Space: 76.2 MB (24.17%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+let array=new Set();
+for(let value of nums){
+    if(array.has(value)){
+      return value;
+    }
+    array.add(value)
+}
+
+
+  
+};
