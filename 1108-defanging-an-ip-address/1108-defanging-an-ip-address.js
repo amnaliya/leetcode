24b9c45a5eebf0e502b2c470a548e73840b2aa79
amnaliya <amnaah207@gmail.com>
2026-09-22 09:47:22 +0530

    Time: 35 ms (89.42%), Space: 54.1 MB (14.42%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,6 +3,6 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    let newest=address.replaceAll(".","[.]");
-    return newest;
+    return address.replaceAll(".","[.]");
+    
 };
